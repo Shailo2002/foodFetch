46d111a18be6eda4d51202808f5c269f420b3c82
shailesh <shailesh364465@gmail.com>
2025-09-25 17:08:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/Signup";
+
+
+
+function App() {
+
+ return (
+   <BrowserRouter>
+     <Routes>
+       <Route path="/signin" element={<SignIn />} />
+       <Route path="/signup" element={<SignUp />} />
+     </Routes>
+   </BrowserRouter>
+ );
+}
+
+export default App
