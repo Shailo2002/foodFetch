9efddaed01cece862a4eb4d584c7d88dc150b4de
shailesh <shailesh364465@gmail.com>
2025-10-05 12:32:12 +0530

    live map section added in checkout page

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -7,26 +7,80 @@ import { Input } from "../ui/Input";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useDispatch, useSelector } from "react-redux";
 import "leaflet/dist/leaflet.css";
-import { setLocation } from "../redux/mapSlice";
+import { setAddress, setLocation } from "../redux/mapSlice";
+import axios from "axios";
 
 function CheckOut() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [addressInput, setAddressInput] = useState("");
   const { location, address } = useSelector((state) => state.map);
+  
+  const apikey = import.meta.env.VITE_GEOAPIKEY;
 
   function RecenterMap({ location }) {
-    if (location?.lat && location?.long) {
-      const map = useMap();
-      map.setView([location?.lat, location?.long], 16, { animate: true });
-    }
+    const map = useMap();
+
+    useEffect(() => {
+      if (!location?.lat || !location?.long) return;
+
+      async function updateMapAndAddress() {
+        map.flyTo([location.lat, location.long], 16, { animate: true });
+
+        try {
+          const res = await axios.get(
+            `https://api.geoapify.com/v1/geocode/reverse?lat=${location.lat}&lon=${location.long}&format=json&apiKey=${apikey}`
+          );
+          const formatted = res?.data?.results?.[0]?.formatted;
+          if (formatted) dispatch(setAddress(formatted));
+        } catch (err) {
+          console.error("Reverse geocode failed:", err);
+        }
+      }
+
+      updateMapAndAddress();
+    }, [location]);
+
     return null;
   }
 
   const onDragEnd = (e) => {
-    console.log(e?.target?._latlng);
     const location = e?.target?._latlng;
     dispatch(setLocation({ lat: location?.lat, long: location?.lng }));
   };
+
+  function getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      dispatch(setLocation({ lat: latitude, long: longitude }));
+    });
+  }
+
+  const getLatLongByAddress = async () => {
+    try {
+      console.log("result : ");
+
+      const result = await axios.get(
+        `https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(
+          addressInput
+        )}&format=json&apiKey=${apikey}`
+      );
+      console.log("result : ", result?.data?.results[0]?.formatted);
+      dispatch(
+        setLocation({
+          lat: result?.data?.results[0]?.lat,
+          long: result?.data?.results[0]?.lon,
+        })
+      );
+    } catch (error) {
+      console.log("error :", error);
+    }
+  };
+
+  useEffect(() => {
+    setAddressInput(address);
+  }, [address]);
 
   return (
     <div className="flex justify-center items-center p-6 min-h-screen w-full bg-gradient-to-b from-orange-200 to-white">
@@ -50,12 +104,16 @@ function CheckOut() {
             <Input
               placeholder={"Enter Address"}
               type={"text"}
-              value={address || ""}
+              value={addressInput || ""}
+              onChange={async (e) => setAddressInput(e.target.value)}
             />
-            <button className="bg-[#ff4d30] text-white p-2.25 mb-2.5 rounded-sm hover:bg-[#ff6c4d] transition-colors duration-300 ease-in-out">
-              <IoSearchOutline />
+            <button className="bg-[#ff4d30] text-white p-2.25 mb-2.5 rounded-sm hover:bg-[#ff6c4d] transition-colors duration-300 ease-in-out cursor-pointer">
+              <IoSearchOutline onClick={getLatLongByAddress} />
             </button>
-            <button className="bg-blue-600 text-white p-2.25 mb-2.5 rounded-sm hover:bg-[#517cff] transition-colors duration-300 ease-in-out">
+            <button
+              className="bg-blue-600 text-white p-2.25 mb-2.5 rounded-sm hover:bg-[#517cff] transition-colors duration-300 ease-in-out cursor-pointer"
+              onClick={() => getCurrentLocation()}
+            >
               <TbCurrentLocation />
             </button>
           </div>
@@ -71,7 +129,7 @@ function CheckOut() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   />
-                  <RecenterMap location={location}/>
+                  <RecenterMap location={location} />
                   <Marker
                     position={[location?.lat, location?.long]}
                     draggable
